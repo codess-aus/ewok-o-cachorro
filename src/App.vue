dcff1d7846fa3dfce27326a328a57d03b18454a0
Michelle Mei-Ling Sandford <msandford@outlook.com>
2022-01-15 05:48:34 +0800

    Commit made from VS Code Azure Static Web Apps

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DetectImage msg="¿Tu cachorro se ha unido al lado oscuro?"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DetectImage from './components/DetectImage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DetectImage
   }
 }
 </script>
